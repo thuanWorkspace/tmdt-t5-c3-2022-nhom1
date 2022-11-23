a61c461269feadbce13627903dc0ff8669f57afb
unknown <thuan0373535207@gmail.com>
2022-11-23 09:50:51 +0700

    first commit - initialize project

---
+++ src/main/java/com/example/tmdtnhom1/Tmdtnhom1Application.java
@@ -0,0 +1,13 @@
+package com.example.tmdtnhom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tmdtnhom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tmdtnhom1Application.class, args);
+	}
+
+}
