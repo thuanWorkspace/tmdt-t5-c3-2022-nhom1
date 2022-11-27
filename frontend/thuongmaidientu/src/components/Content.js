cf97dcc3b0171ca31b2591f301d9d7c7b2a783cb
Trieutien2001vn <19130230@st.hcmuaf.edu.vn>
2022-11-28 00:33:52 +0700

    fix

---
+++ frontend/thuongmaidientu/src/components/Content.js
@@ -0,0 +1,4 @@
+function Content() {
+  return <div className="row-span-3"></div>;
+}
+export default Content;
---
+++ frontend/thuongmaidientu/src/components/footer/component/FooterItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function FooterItem() {
+  return (
+    <ul className="">
+      <h3 className="">Fshare</h3>
+      <li className="">Tính năng</li>
+      <li className="">Tính năng</li>
+      <li className="">Tính năng</li>
+      <li className="">Tính năng</li>
+    </ul>
+  );
+}
