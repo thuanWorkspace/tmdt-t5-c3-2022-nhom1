13bfabdea1dc9bd87c058148bac83db7f9f77901
Trieutien2001vn <19130230@st.hcmuaf.edu.vn>
2022-12-16 02:56:01 +0700

    design footer component

--- frontend/thuongmaidientu/src/components/footer/component/FooterItem.js
+++ frontend/thuongmaidientu/src/components/footer/component/FooterItem.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function FooterItem({ name, listItem }) {
   return (
-    <ul className="">
-      <h3 className="">{name}</h3>
+    <ul className="mt-16">
+      <h3 className="font-medium text-md">{name}</h3>
 
       {listItem.map((item, index) => (
-        <li className="" key={index}>
+        <li className="mt-4 cursor-pointer" key={index}>
           {item}
         </li>
       ))}
